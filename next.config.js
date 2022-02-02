09e7dfaf5fc87dadae6548399ffb609ff643a9f4
Rohit967367 <rohitpramanik30593@gmail.com>
2022-02-02 09:17:12 +0530

    add

--- next.config.js
+++ next.config.js
@@ -1,29 +1,10 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = (phase) => {
-  // reactStrictMode: true,
-
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-      },
-      env: {
-        NEXTAUTH_URL: "http://localhost:3000/",
-      },
-    };
-  }
-
-  return {
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-    env: {
-      NEXTAUTH_URL: "http://localhost:3000/",
-    },
-  };
+module.exports = {
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
