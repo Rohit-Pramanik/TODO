77b687c9202585393e38b0c68ee4e8179b20bf46
Rohit-Pramanik <rohitpramanik30593@gmail.com>
2024-01-01 16:13:32 +0530

    envoriment credential update

--- Component/Database/Firebase.js
+++ Component/Database/Firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 var firebaseConfig = {
-  apiKey: process.env.API_KEY || process.env.apiKey,
-  authDomain: process.env.AUTH_DOMAIN || process.env.authDomain,
-  projectId: process.env.PROJECT_ID || process.env.projectId,
-  storageBucket: process.env.STORAGE_BUCKET || process.env.storageBucket,
-  messagingSenderId: process.env.MESSAGEING_SENDING_ID || process.env.messagingSenderId,
-  appId: process.env.APP_ID || process.env.appId,
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 const app = initializeApp(firebaseConfig);
--- next.config.js
+++ next.config.js
@@ -2,16 +2,15 @@ module.exports = () => {
   return {
     reactStrictMode: true,
     env: {
-      api: "123",
-      apiKey: "AIzaSyB-AEup-yOl6EM0dhhuKE04uYhEvDRh0_M",
-      authDomain: "todo-20106.firebaseapp.com",
-      projectId: "todo-20106",
-      storageBucket: "todo-20106.appspot.com",
-      messagingSenderId: "421894344563",
-      appId: "421894344563:web:60b2a4d785fe5bd3153f06",
-      userName: "Deadpool",
-      clientId: "421894344563-unl4lrdkd0huud9hojnv6buf943j4m5o.apps.googleusercontent.com",
-      clientSecretKey: "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"
+      apiKey: process.env.NEXT_PUBLIC_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_MESSAGEING_SENDING_ID,
+      appId: process.env.NEXT_PUBLIC_APP_ID,
+      userName: process.env.NEXT_PUBLIC_USER_NAME,
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecretKey: process.env.NEXT_PUBLIC_CLIENT_SECRET,
     },
     eslint: {
       ignoreDuringBuilds: true,
